25a283a5d3113f70546597938ca48d6745ac4af0
ajilll31 <azrielrosadi03@gmail.com>
2025-06-26 14:24:01 +0700

    Memperbaiki Hero Section yang Menabrak Navbar

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -25,14 +25,14 @@ const Hero = () => {
   });
 
   return (
-    <section id="hero" className="relative overflow-hidden">
+    <section id="hero" className="relative overflow-hidden md:pt-24">
       <div className="absolute top-0 left-0 z-10">
         <img src="/images/bg.png" alt="background" />
       </div>
 
       <div className="hero-layout">
         {/*LEFT: HERO CONTENT */}
-        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5 min-h-screen">
           <div className="flex flex-col gap-7 overflow-x-clip">
             <div className="hero-text">
               <h1>
